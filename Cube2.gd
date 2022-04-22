213acfa626471e1863729afa69569c7226768f3e
Teodor Potancok <teodor.potancok@gmail.com>
2022-04-22 20:42:56 +0200

    Now works with any transform!

--- Cube2.gd
+++ Cube2.gd
@@ -5,10 +5,11 @@ func random_points_in_box(amount: int, start: Vector3, end: Vector3, p_seed := "
 		seed(hash(p_seed))
 	var array = PackedVector3Array()
 	for i in range(amount):
-		array.append(Vector3(
+		var vec = Vector3(
 		randf_range(start.x - margin, end.x + margin),
 		randf_range(start.y - margin, end.y + margin),
-		randf_range(start.z - margin, end.z + margin)))
+		randf_range(start.z - margin, end.z + margin))
+		array.append(vec)
 	return array
 	
 func _ready():
@@ -17,11 +18,19 @@ func _ready():
 func slice():
 	var meshinstance = $MeshInstance3D
 	var voro = Voronoi.new()
-	var bounds = meshinstance.get_aabb()
+	var bounds = meshinstance.get_aabb().abs()
+	
+	var og_xform = global_transform
 	
 	voro.setup(bounds.position, bounds.end)
-	voro.set_points(random_points_in_box(10, bounds.position + position, bounds.end + position))
+	var points = random_points_in_box(10, bounds.position, bounds.end)
+	for point in points:
+		if point > (bounds.end) or point < (bounds.position):
+			print("Point out of bounds!")
+	
+	voro.set_points(points)
 	voro.compute()
+#	problem above this line
 #	var face1 = voro.get_face(0, 0)
 #	var face2 = voro.get_face(0, 1)
 
@@ -36,9 +45,12 @@ func slice():
 		var mesh = $MeshInstance3D.duplicate()
 		var body = RigidDynamicBody3D.new()
 		
+		
 		var line = file.get_line()
 		
 		var split = line.split(";")
+		mesh.global_transform = Transform3D()
+		
 		if split.size() == 3:
 			var point_str = split[0].split(" ")
 			var point = Vector3(point_str[0].to_float(), point_str[1].to_float(), point_str[2].to_float())
@@ -57,12 +69,10 @@ func slice():
 				
 				if face.size() >= 3:
 					
-					
 					var plane = Plane(positions[face[0].to_int()], positions[face[1].to_int()], positions[face[2].to_int()])
 					
 					if plane.is_point_over(point):
 						plane = -plane
-						print("plane inverted")
 
 					var mat = StandardMaterial3D.new()
 					mat.albedo_color = Color(randf(), randf(), randf())
@@ -71,8 +81,10 @@ func slice():
 					var sliced = slicer.slice_by_plane(mesh.mesh, plane, mat)
 					if sliced:
 						mesh.mesh = sliced.lower_mesh
-			body.add_child(mesh)
-			body.freeze = true
-			get_parent().call_deferred("add_child", body)
+			if mesh.mesh != $MeshInstance3D.mesh:
+				body.global_transform = og_xform
+				body.add_child(mesh)
+				body.freeze = true
+				get_parent().call_deferred("add_child", body)
 			
 	queue_free()
